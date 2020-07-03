6832fd29097116ca88ce7112e3e268885d745504
Rocha Lima <jonasrla@gmail.com>
2020-07-03 15:25:14 -0300

    cria modelo de professores

--- restful_api/professores/models.py
+++ restful_api/professores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    nascimento = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
