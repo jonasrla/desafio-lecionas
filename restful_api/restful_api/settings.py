27742a0db066a2b29b6764e2bc5dbe39220f75ab
Rocha Lima <jonasrla@gmail.com>
2020-07-03 21:55:51 -0300

    setup do mysql

--- restful_api/restful_api/settings.py
+++ restful_api/restful_api/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'restful_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/Users/Jonas/git_storage/desafio-lecionas/restful_api/config/db_config.cnf',
+        },
+        #'NAME': os.path.join(BASE_DIR, 'db.mysql'),
     }
 }
 
