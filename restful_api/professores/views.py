6895d6b454df339443ac9634f07fcfb8684da67d
Rocha Lima <jonasrla@gmail.com>
2020-07-03 22:10:56 -0300

    Adiciona view

--- restful_api/professores/views.py
+++ restful_api/professores/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from professores.models import Professor
+
+class BookList(ListView):
+    model = Professor
+
+class BookView(DetailView):
+    model = Professor
+
+class BookCreate(CreateView):
+    model = Professor
+    fields = ['nome','email','nascimento']
+    success_url = reverse_lazy('book_list')
+
+class BookUpdate(UpdateView):
+    model = Professor
+    fields = ['nome','email','nascimento']
+    success_url = reverse_lazy('book_list')
+
+class BookDelete(DeleteView):
+    model = Professor
+    success_url = reverse_lazy('book_list')
